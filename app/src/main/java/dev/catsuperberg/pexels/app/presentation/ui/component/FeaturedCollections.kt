b6c49a714c3064105002fb6ebb7c6f7ba26ff69d
CATSUPERBERG <mixer468@gmail.com>
2023-11-29 14:01:52 +0300

    HS: AC15, AC17 Collection reordering on selection

--- app/src/main/java/dev/catsuperberg/pexels/app/presentation/ui/component/FeaturedCollections.kt
+++ app/src/main/java/dev/catsuperberg/pexels/app/presentation/ui/component/FeaturedCollections.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
@@ -25,18 +27,30 @@ fun FeaturedCollections(
 ) {
     val collections = viewModel.collections.collectAsState()
     val selected = viewModel.collection.collectAsState()
+    val listState = rememberLazyListState()
 
     val present = remember { derivedStateOf { collections.value.isNotEmpty() } }
+    val collectionOrder = remember {
+        derivedStateOf {
+            selected.value?.let { selectedIndex ->
+                listOf(selectedIndex) + collections.value.indices.filter { it != selectedIndex }
+            } ?: collections.value.indices.toList()
+        }
+    }
+
+    LaunchedEffect(collectionOrder.value) { listState.animateScrollToItem(0) }
 
     if (present.value) {
         LazyRow(
             horizontalArrangement = Arrangement.spacedBy(12.dp),
+            state = listState,
             contentPadding = contentPadding,
             modifier = modifier
                 .fillMaxWidth()
                 .height(38.dp)
         ) {
-            items(collections.value.count()) { index ->
+            items(collections.value.count()) { displayIndex ->
+                val index = collectionOrder.value[displayIndex]
                 Button(
                     onClick = { viewModel.onCollectionSelected(index) },
                     colors = if (selected.value == index) ButtonDefaults.buttonColors(
